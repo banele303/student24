3c40f5a58b1add9755c81351d1ee125f763ade9b
alex banele <alexsouthflow@gmail.com>
2025-05-24 10:16:55 +0200

    blessed mess

--- src/app/admin/landlords/[id]/client.tsx
+++ src/app/admin/landlords/[id]/client.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import React, { useState } from "react";
 import { useRouter } from "next/navigation";
 import { useTheme } from "next-themes";
 import { cn } from "@/lib/utils";
